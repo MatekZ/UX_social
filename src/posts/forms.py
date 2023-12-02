1806b7814c9b3a949f96b72c4367555772f56348
Kaspr-whtev <60173961+Kaspr-whtev@users.noreply.github.com>
2023-12-02 16:19:00 +0100

    przetłumaczenie strony na polski + powiększenie przycisku do dodania zdjecia do posta

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Post, Comment
 
 class PostModelForm(forms.ModelForm):
+    post_content = forms.CharField(label='Tekst Posta:', widget=forms.Textarea())
+    image = forms.CharField(label='Zdjęcie', widget=forms.FileInput(attrs={'style':'font-size: 1.55vh'}))
     class Meta:
         model = Post
         fields = ('post_content', 'image')
